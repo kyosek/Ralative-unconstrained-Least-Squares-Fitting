3baa1dadcdb409bf6f36fb2aafda06fde484ac6c
kyosuke.morita <kyosuke.morita@gamesys.co.uk>
2020-04-05 22:05:50 +0100

    changed output

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='rulsif',
-    version='0.2.2.9000',
+    version='1.0.0.1',
     description='A Python Package for Density Ratio Estimation',
     url='https://github.com/kyosek/Ralative-unconstrained-Least-Squares-Fitting.git',
     author='Koji Makiyama, Ameya Daigavane',
